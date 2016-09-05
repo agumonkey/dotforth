64e30a528fb1bcb0455abf089fb91d8ce561ab27
Onat Turkcuoglu <onattoglu@gmail.com>
2016-09-05 20:22:02 +0300

    Cleanup

--- host.c
+++ host.c
@@ -8,9 +8,7 @@ int main(int argc, char** argv)
     static char BytecodeStream[Kilobytes(64)];
     static int CommandBuffer[128];
     load_file_result Result = LoadFileIntoMemory("bytecode", BytecodeStream, sizeof BytecodeStream);
-    printf("Hello world!\n");
     dotForthExecute(BytecodeStream, CommandBuffer);
-    printf("BYTE world!\n");
 
     for (int ii=0; ii<2; ++ii)
     {
